c0b3cf7f29edfa04938bfe06cb3923dab4bbbed6
Eduir Brazon <eduirbrazon@gmail.com>
2023-01-30 10:46:05 -0400

    bugfix  at hhhh.js

--- apps/form/static/js/hhhh.js
+++ apps/form/static/js/hhhh.js
@@ -1,48 +1,75 @@
-var jso = {};
+console.log("js loaded");
+var js;
+var cont = 0;
+var tfeel = 0;
+var trole = 0;
+const FeelRole = (value) => {
+  console.log(typeof value);
+  console.log(value);
+  value = JSON.parse(value);
+  console.log(typeof value);
+  tfeel += value.feel;
+  trole += value.role;
+};
 const Fetchestractor = (json) => {
   jso = json;
 };
 const fe = async (url = "http://localhost:8000/question") => {
   try {
     const quest = await fetch(url);
-    if (quest.status == 200) {
-      let js = await quest.json();
-      Fetchestractor(js);
+    if (quest.ok) {
+      js = await quest.json();
+
+      writeQuestion();
+      buildBoton();
     }
   } catch (error) {
     console.log(error);
   }
 };
 fe();
-var cont = 0;
-
 function writeQuestion() {
   var question = document.getElementById("question");
-  question.innerText = jso[cont].question;
-  console.log(jso[cont].question);
+  question.innerText = js[cont].question;
   let answers = "";
-  for (let answer of jso[cont].answers) {
+  for (let answer of js[cont].answers) {
     answers += `
-          <input type="radio" value="{'feel':${answer.feel},'role':${answer.role}}" name="ans">${answer.answer}<br>
+          <input type="radio" value='{"feel":${answer.feel}, "role":${answer.role}}' name="ans">${answer.answer}<br>
           `;
   }
   var ans = document.getElementById("answer");
   ans.innerHTML = answers;
 }
-var start = document.getElementById("answer");
-start.innerHTML = ` `;
-let boto = document.getElementById("next");
-boto.addEventListener("click", function () {
-  console.log(jso);
-  //   try {
-  //     let check = document.querySelector('input[name="ans"]:checked').value;
-  //     console.log(check);
-  //     cont++;
-  //     writeQuestion();
-  //   } catch {
-  //     alert("none selected");
-  //   }
-});
-//writeQuestion();
+
+const buildBoton = () => {
+  const boto = document.getElementById("next");
+  boto.addEventListener("click", function () {
+    try {
+      const check = document.querySelector('input[name="ans"]:checked').value;
+      console.log(check);
+      FeelRole(check);
+      cont++;
+      console.log(tfeel, trole);
+      if (cont == js.length - 1) {
+        boto.setAttribute("value", "SIMMIT");
+      }
+      cont < js.length ? writeQuestion() : result();
+    } catch (err) {
+      console.log(err);
+      alert("Seleccione una Respuesta");
+    }
+  });
+};
+
+const result = () => {
+  document.getElementById("question").remove();
+  document.getElementById("next").remove();
+  var ans = document.getElementById("answer");
+  ans.innerHTML = `<h1>Result<h1/><br>
+  <p><b>Feel: <b/> ${tfeel}<br>
+  <b>Role: <b/>${trole}
+  <p/>
+  `;
+};
 
 const info = () => {};
--- apps/form/urls.py
+++ apps/form/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from .views import *
 
 router = DefaultRouter(trailing_slash=False)
-router.register('create-question', QuestionSet)
-router.register('create-answer', AnswerSet)
+# router.register('create-question', QuestionSet)
+# router.register('create-answer', AnswerSet)
 router.register('question', ShowQuestions)
 urlpatterns = [
     path('', index, name='home'),
